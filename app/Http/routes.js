0c3df3135d856c42bb319a51b39a9c3c489ebec2
Brannon Crumpton <bcrump@daniel-johnsons-macbook-pro.local>
2017-03-14 13:07:04 -0500

    POST, PUT and GET functions in Adonis

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -15,6 +15,91 @@
 | Route.resource('user', 'UserController')
 */
 
-const Route = use('Route')
+const Route = use('Route');
+const fetch = require('node-fetch');
 
-Route.on('/').render('welcome')
+// Route.on('/').render('welcome');
+
+// async function() {
+//   const res = await fetch('http://swapi.co/people/1');
+//   const luke = await res.json();
+//
+//   response.json(luke);
+// }
+
+// Route.get('/', function * (request, response) {
+//   const res = yield fetch('http://swapi.co/api/people/1');
+//   const luke = yield res.json();
+//
+//   response.json(luke);
+// });
+
+// const Database = require('knex')(config.development);
+const Database = use('Database');
+
+Route.get('/restaurants', function * (request, response) {
+  const restaurants = yield Database.select().from('restaurants');
+
+  response.send(items);
+});
+
+Route.get('/restaurants/:id', function * (request, response) {
+  const id = request.param('id');
+
+  const r = yield Database.select().from('restaurants')
+    .where({ id: id })
+    .limit(1)
+    // This is getting the first item from the result
+    .first();
+
+  // Check if the restaurant exists
+  if (r === undefined) {
+    // Send the status code 404 (not found) witha a JSON error object
+    return response.status(404).json({
+      err: 'Not found'
+    });
+  }
+
+  response.send(r);
+});
+
+Route.post('/restaurants', function * (request, response) {
+  const restaurant = {
+    name: request.input('name'),
+    category: request.input('category'),
+    wait_time: request.input('wait_time'),
+    take_out: request.input('take_out'),
+    formal: request.input('formal'),
+    address: request.input('address'),
+    flair: request.input('flair'),
+    price_level: request.input('price_level'),
+  }
+
+  yield Database.insert(restaurant).into('restaurants');
+
+  response.send(restaurant);
+});
+
+Route.put('/restaurants/:id', function * (request, response) {
+  const id = request.param('id');
+
+  const r = yield Database.select().from('restaurants')
+    .where({ id: id })
+    .limit(1)
+    // This is getting the first item from the result
+    .first();
+
+  // Check if the restaurant exists
+  if (r === undefined) {
+    // Send the status code 404 (not found) witha a JSON error object
+    return response.status(404).json({
+      err: 'Not found'
+    });
+  }
+
+  const input = request.only('name', 'category', 'wait_time', 'take_out', 'formal', 'address', 'flair', 'price_level');
+
+  yield Database.table('restaurants').update(input);
+
+  response.send(input);
+});
